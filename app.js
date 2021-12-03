fc04f4b82997707e771e32370480e4eaa501afca
unknown <kellskelechi@gmail.com>
2021-12-03 14:05:41 +0100

    header-updated

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
+/* eslint-disable no-unused-expressions */
+$(document).ready(function () {
+  $("#menu").click(function () {
+    $(this).toggleClass("fa-times");
 
-$(document).ready(function(){
+    $(".navbar").toggleClass("nav-toggle");
+  });
 
-    
-    $('#menu').click(function(){
-        
-        $(this).toggleClass('fa-times');
-        
-        $('.navbar').toggleClass('nav-toggle');
-    });
-})
+  $(window).on('scroll load',function(){
+    $("#menu").removeClass("fa-times");
 
+    $(".navbar").removeClass("nav-toggle");
+
+
+  });
+});
