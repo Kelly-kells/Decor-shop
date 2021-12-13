6cccd68d6a76aec4a56a5f3bd6e5b0559a1b7aba
unknown <kellskelechi@gmail.com>
2021-12-13 10:59:38 +0100

    about-sectionEnds

--- app.js
+++ app.js
@@ -23,5 +23,16 @@ $(document).ready(function () {
     }else{
       $('header').removeClass('header-active')
     }
+    $('section').each(function(){
+      let top=$(window).scrollTop();
+      let height =$(this).height();
+      let offset=$(this).offset().top - 200;
+      let id = $(this).attr('id')
+
+      if (top >= offset && top < offset + height) {
+         $('.navbar ul li a').removeClass('active');
+         $('navbar').find(`[href="#${id}"]`).addClass('active');
+      }
+    })
   });
 });
