5cbda672ac1ef0d9a4711b40404d5bd3578897e2
unknown <kellskelechi@gmail.com>
2022-01-27 15:43:25 +0100

    responsive

--- app.js
+++ app.js
@@ -39,8 +39,12 @@ function showroomScroll() {
 lastScroll.addEventListener('click', showroomScroll)
 
 
-function menuToggle() {
-  var menu=document.getElementById('#menu')
-  menu.classList.toggle('heading #menu')
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
 }
 
